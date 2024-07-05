c2e2d1c20a8d7e8fcd9350509aeb706fe6ac05b9
AadityaAwasthy <aadityaawasthy02@gmail.com>
2024-07-05 15:10:59 +0530

    converted the server to multithreaded program

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use std::{net::{TcpListener, TcpStream}, io::{BufReader, BufRead, Write}, fs};
+use std::{net::{TcpListener, TcpStream}, io::{BufReader, BufRead, Write}, fs, thread, time::Duration};
+use server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").expect("The server could not bind to the specified address");
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.expect("Error while opening a connection");
-        handle_connection(stream);
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -15,10 +20,13 @@ fn handle_connection(mut stream: TcpStream) {
 
     println!("Request: {stream:#?}");
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(format!("./html/{filename}")).unwrap();
